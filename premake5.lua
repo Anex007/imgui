c2541c21668e67e52fdc529e04f56960f253b848
Anex007 <anexjohnson007@gmail.com>
2020-08-09 17:30:36 -0400

    Modified to usable submodule

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.cpp",
+        "imgui.h",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        -- "examples/imgui_impl_opengl3.cpp",
+        -- "examples/imgui_impl_opengl3.h",
+        -- "examples/imgui_impl_glfw.cpp",
+        -- "examples/imgui_impl_glfw.h",
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "on"
+
+    filter { "configurations:Debug" }
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter { "configurations:Release" }
+        defines { "NDEBUG" }
+        optimize "On"
